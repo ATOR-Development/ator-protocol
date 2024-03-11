b346a0fb8bc37e829338c41313a5f514a72a7792
Sergey Klimuk <sergey.klimuk@fortegrp.com>
2024-03-11 18:41:20 +0300

    live env prep

--- operations/uploadsecrets.sh
+++ operations/uploadsecrets.sh
@@ -1,6 +1,6 @@
-#arguments folder node-id
+#arguments folder node-id env
 
-vault kv put -output-curl-string -mount=kv ator-network/stage/dir-auth-$2 \
+vault kv put -output-curl-string -mount=kv ator-network/$3/dir-auth-$2 \
             authority_identity_key="$(cat $1/tor-data/keys/authority_identity_key)" \
             authority_signing_key="$(cat $1/tor-data/keys/authority_signing_key)" \
             ed25519_master_id_secret_key_base64="$(base64 -w 0 $1/tor-data/keys/ed25519_master_id_secret_key)" \
